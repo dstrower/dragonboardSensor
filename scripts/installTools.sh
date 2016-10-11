0f5061ca2ab13f975fc9c8d10990067007e42983
David Trower <dstrower@gmail.com>
2016-10-11 14:05:58 +0000

    fixed script

--- scripts/installTools.sh
+++ scripts/installTools.sh
@@ -1,2 +1,2 @@
-sudo apt-get install arduino-mk arduino git build-essential autoconf libtool swig3.0 python-dev nodejs-dev cmake pkg-config libpcre3-dev
+sudo apt-get -f install arduino-mk arduino git build-essential autoconf libtool swig3.0 python-dev nodejs-dev cmake pkg-config libpcre3-dev
 sudo apt-get clean
