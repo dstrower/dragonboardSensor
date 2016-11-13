92ad47ff29db2c21c11288d2b9006adf81094936
dstrower <dstrower@gmail.com>
2016-11-13 15:46:45 +0000

    Initial checkin

---
+++ scripts/96boards-sensors.sh
@@ -0,0 +1,6 @@
+# Environmental variables for working with the 96Boards Sensors adapter
+#
+# Copy this file into /etc/profile.d to set up the correct environment by default
+export JAVA_TOOL_OPTIONS="-Dgnu.io.rxtx.SerialPorts=/dev/tty96B0"
+export MONITOR_PORT=/dev/tty96B0
+export PYTHONPATH="$PYTHONPATH:/usr/local/lib/python2.7/site-packages"
