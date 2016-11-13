2600b9cec268d0d8ba8bfec3b091684d71be5379
dstrower <dstrower@gmail.com>
2016-11-13 12:14:10 +0000

    Changed install instructions

--- scripts/installTools.sh
+++ scripts/installTools.sh
@@ -1,2 +1,3 @@
-sudo apt-get -f install arduino-mk arduino git build-essential autoconf libtool swig3.0 python-dev nodejs-dev cmake pkg-config libpcre3-dev
+sudo apt-get install arduino-mk arduino git build-essential autoconf swig3.0 python-dev nodejs-dev cmake pkg-config libpcre3-dev automake libtool
+sudo apt-get install libmraa-dev libupm-dev
 sudo apt-get clean
--- scripts/updateDebian.sh
+++ scripts/updateDebian.sh
@@ -1,4 +1,3 @@
 ntpdate pool.ntp.org
 sudo apt-get update
 yes|sudo apt-get dist-upgrade -u
-sudo apt-get install libmraa-dev libupm-dev
