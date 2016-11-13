6989ac010710d7cea07c069fb602073d0ddb24f6
dstrower <dstrower@gmail.com>
2016-11-13 15:48:50 +0000

    Fixed configTools.sh

--- scripts/configSoftware.sh
+++ scripts/configSoftware.sh
@@ -1,9 +1,5 @@
 sudo adduser linaro i2c # Allow the normal user to perform i2c operations
 git clone https://github.com/96boards/96boards-tools
 sudo cp 96boards-tools/70-96boards-common.rules /etc/udev/rules.d/
-cat | sudo tee /etc/profile.d/96boards-sensors.sh << EOF
-export JAVA_TOOL_OPTIONS="-Dgnu.io.rxtx.SerialPorts=/dev/tty96B0"
-export MONITOR_PORT=/dev/tty96B0
-export PYTHONPATH="$PYTHONPATH:/usr/local/lib/python2.7/site-packages"
-EOF
+sudo cp 96boards-sensors.sh /etc/profile.d
 sudo cp /etc/profile.d/96boards-sensors.sh /etc/X11/Xsession.d/96boardssensors
