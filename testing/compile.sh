defdfc57b6c1f5d9d61052efe5af1eb483950dd7
linaro <linaro@linaro-alip>
2024-11-27 16:12:32 +0000

    Got the java servo progam to work with one servo.

--- testing/compile.sh
+++ testing/compile.sh
@@ -1,1 +1,1 @@
-javac -classpath /usr/share/java/upm_jhd1313m1-1.6.0.jar:/usr/share/java/upm_lsm6ds3h.jar:../jars/jSerialComm.jar:. $1
+javac -classpath /usr/share/java/upm_jhd1313m1-1.6.0.jar:/usr/share/java/upm_lsm6ds3h.jar:/usr/share/java/jSerialComm.jar:. $1
--- testing/runProg.sh
+++ testing/runProg.sh
@@ -1,1 +1,1 @@
-java -classpath /usr/share/java/upm_jhd1313m1-1.6.0.jar:/usr/share/java/upm_lsm6ds3h.jar:. $1
+java -classpath /usr/share/java/upm_jhd1313m1-1.6.0.jar:/usr/share/java/upm_lsm6ds3h.jar:/usr/share/java/jSerialComm.jar:. $1
