0d41502d3ad1e7bf3e3b20daaaa72712b28f24e2
linaro <linaro@linaro-alip>
2024-12-17 13:28:09 +0000

    Made changes.

---
+++ server/ArduinoSerial.java
@@ -0,0 +1,33 @@
+import com.fazecast.jSerialComm.*;
+
+import java.io.OutputStream;
+import java.util.Scanner;
+
+public class ArduinoSerial implements Arduiono {
+    public void sendMessage(String messagge) {
+        // Open the serial port
+        SerialPort comPort = SerialPort.getCommPort("/dev/tty96B0"); // Replace with your port name
+        comPort.openPort();
+
+        // Set up the port parameters
+        comPort.setBaudRate(9600);
+        comPort.setNumDataBits(8);
+        comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
+        comPort.setParity(SerialPort.NO_PARITY);
+        // Write to the serial port
+        try {
+            OutputStream outputStream = comPort.getOutputStream();
+                outputStream.write(message.getBytes());
+                outputStream.write(System.getProperty("line.separator").getBytes());
+                outputStream.flush();
+
+           }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        // Close the serial port
+        comPort.closePort();
+    }
+}
+
