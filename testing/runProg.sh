c42a8203e548835a80194b229c8862ceda2aabeb
linaro <linaro@linaro-alip>
2024-12-21 16:03:59 +0000

    Made changes to scripts to support new jar file.

--- testing/runProg.sh
+++ testing/runProg.sh
@@ -1,1 +1,1 @@
-java -classpath /usr/share/java/upm_jhd1313m1-1.6.0.jar:/usr/share/java/upm_lsm6ds3h.jar:/usr/share/java/jSerialComm.jar:. $1
+java -classpath /usr/share/java/upm_jhd1313m1-1.6.0.jar:/usr/share/java/upm_lsm6ds3h.jar:/usr/share/java/jSerialComm.jar:/usr/share/java/temperature-1.0.0.jar:. $1
