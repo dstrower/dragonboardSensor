3f54d6d76e63267dbe08b982b9a4e28d76b08143
Trower <David.Trower@ehi.com>
2024-12-23 11:56:56 -0600

    Made changes.

--- server/DataHolder.java
+++ server/DataHolder.java
@@ -21,6 +21,9 @@ public class DataHolder {
   private List<Float> xVelocityList = new ArrayList<>();
   private List<Float> yVelocityList = new ArrayList<>();
   private List<Float> zVelocityList = new ArrayList<>();
+  private List<Float> xPositionList = new ArrayList<>();
+  private List<Float> yPositionList = new ArrayList<>();
+  private List<Float> zPositionList = new ArrayList<>();
   private List<Float> timeElapseList = new ArrayList<>();
 
 
@@ -31,6 +34,12 @@ public class DataHolder {
   private float xVelocityCurrent = 0.0F;
   private float yVelocityCurrent = 0.0F;
   private float zVelocityCurrent = 0.0F;
+  private float xVelocityAverage = 0.0F;
+  private float yVelocityAverage = 0.0F;
+  private float zVelocityAverage = 0.0F;
+  private float xPosition = 0.0F;
+  private float yPosition = 0.0F;
+  private float zPosition = 0.0F;
   int count = 0;
   public void addPoint(float timeElapse, floatVector accelData, floatVector gyroData, float tempC, float tempF) {
     float xAcceleration = accelData.get(0) - xOffset;
@@ -51,6 +60,17 @@ public class DataHolder {
     xVelocityList.add(xVelocityCurrent);
     yVelocityList.add(yVelocityCurrent);
     zVelocityList.add(zVelocityCurrent);
+    if(timeElapse >0) {
+      xVelocityAverage = (xVelocityList.get(count) + xVelocityList.get(count-1))/2.0F;
+      yVelocityAverage = (yVelocityList.get(count) + yVelocityList.get(count-1))/2.0F;
+      zVelocityAverage = (zVelocityList.get(count) + zVelocityList.get(count-1))/2.0F;
+    }
+    xPosition = xPosition + xVelocityAverage*timeElapse;
+    yPosition = yPosition + yVelocityAverage*timeElapse;
+    zPosition = zPosition + zVelocityAverage*timeElapse;
+    xPositionList.add(xPosition);
+    yPositionList.add(yPosition);
+    zPositionList.add(zPosition);
     count++;
   }
 
